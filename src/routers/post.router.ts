2822e20bec8c2fa5c21c4d52a616afd0cc103078
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-05-01 01:55:56 +0300

    secure generator router

--- src/routers/post.router.ts
+++ src/routers/post.router.ts
@@ -1,9 +1,9 @@
 import express = require('express');
 import { TrendBlogPostDAO } from '../DAO/postDAOs';
-// import { generateBlogPost } from '../services/gemini/generateBlogPost';
-// import { mapRequestToTrend } from '../services/gemini/mapRequestToTrend';
-// import { fetchTrendingSearches } from '../services/google-trend/fetchTrendingSearches';
-// import { TrendBlogPost } from '../common/GoogleTrend.interface';
+import { generateBlogPost } from '../services/gemini/generateBlogPost';
+import { mapRequestToTrend } from '../services/gemini/mapRequestToTrend';
+import { fetchTrendingSearches } from '../services/google-trend/fetchTrendingSearches';
+import { TrendBlogPost } from '../common/GoogleTrend.interface';
 
 const router = express.Router();
 const blogPostDAO = new TrendBlogPostDAO();
@@ -12,6 +12,36 @@ const blogPostDAO = new TrendBlogPostDAO();
 router.get('/', async (req: express.Request, res: express.Response) => {
     const posts = await blogPostDAO.read();
     res.json(posts);
+});
+
+// TODO: 1- optimize performance by using a queue to generate blog posts
+// 2- add a cron job to generate blog posts every 24 hours
+// 3- make this function idempotent
+router.get('/generate', async (req: express.Request, res: express.Response) => {
+    console.log(req.headers.authorization, process.env.GENERATOR_BASIC_AUTH);
+    
+    if (req.headers.authorization!== process.env.GENERATOR_BASIC_AUTH) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+    const TrendingBlogPosts: Array<TrendBlogPost> = [];
+
+    const trendingFetched = await fetchTrendingSearches({
+        country_iso: "US",
+        category: "all",
+    });
+
+    const trending = mapRequestToTrend(trendingFetched);
+
+    for await (const trend of trending) {
+        const blogPost = await generateBlogPost(trend.title);
+        TrendingBlogPosts.push({ ...trend, blogPost });
+    }
+    // save to db
+    await blogPostDAO.insertMany(TrendingBlogPosts);
+    res.json({
+        message: "Trending blog posts generated",
+    });
 });
 
 
@@ -40,30 +70,6 @@ router.delete('/:id', async (req: express.Request, res: express.Response) => {
 });
 
 
-router.get('/generate', async (req: express.Request, res: express.Response) => {
-    console.log(req.headers.authorization, process.env.GENERATOR_BASIC_AUTH);
-    
-    if (req.headers.authorization!== process.env.GENERATOR_BASIC_AUTH) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-    // const TrendingBlogPosts: Array<TrendBlogPost> = [];
 
-    // const trendingFetched = await fetchTrendingSearches({
-    //     country_iso: "US",
-    //     category: "all",
-    // });
-
-    // const trending = mapRequestToTrend(trendingFetched);
-
-    // for await (const trend of trending) {
-    //     const blogPost = await generateBlogPost(trend.title);
-    //     TrendingBlogPosts.push({ ...trend, blogPost });
-    // }
-    // // save to db
-    // await blogPostDAO.insertMany(TrendingBlogPosts);
-    return res.json({
-        message: "Trending blog posts generated",
-    });
-});
 
 export default router;
