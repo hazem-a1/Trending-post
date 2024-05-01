006558427a5248566fed2d7d4c5478dc78309c7f
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-05-01 04:42:10 +0300

    add port to env

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { ClientProvider } from './db/ClientProvider';
 import postRouter from './routers/post.router';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
