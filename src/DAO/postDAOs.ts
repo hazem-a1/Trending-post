03f8f946088b1964a519ca5e7cd683efb5f2d053
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-05-14 14:21:22 +0300

    create index

--- src/DAO/postDAOs.ts
+++ src/DAO/postDAOs.ts
@@ -4,7 +4,13 @@ import { dbCollections } from '../constant/db-collections';
 
 export class TrendBlogPostDAO extends MongoDbCollection<TrendBlogPost> {
   constructor() {
-    super(dbCollections.posts); 
+    super(dbCollections.posts);
+    // Create a text index on title and content fields
+    this.collection.createIndex({ title: "text", blogPost: "text"  }, { background: true }).then((res) => {
+      console.log(`Text index created ${res}`);
+    }).catch((error) => {
+      console.log('Error creating text index', error);
+    });
   }
 }
 
