94b4be1b255bb4a9f72668ed5719b80a3a3edd59
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-07-14 21:36:26 +0300

    switch to block none safty

--- src/services/gemini/generateBlogPost.ts
+++ src/services/gemini/generateBlogPost.ts
@@ -5,19 +5,19 @@ export const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_API_KE
 const safetySettings = [
    {
       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
   },
    {
       category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
   },
    {
       category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
   },
    {
       category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
   },
 ];
 
